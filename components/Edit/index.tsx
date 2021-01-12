f7a2cbf99af0e5cb463346662cd72f6fc4fb3c04
harutete <ai.takeuchi.10@gmail.com>
2021-01-13 00:20:20 +0900

    アップロードした画像のサムネイルを表示するメソッドを追加

--- components/Edit/index.tsx
+++ components/Edit/index.tsx
@@ -1,26 +1,42 @@
 import Link from 'next/link'
 import Layout from '../common/Layout'
+import { useState } from 'react'
 
-const uploadImage = () => {
-  console.log('hoge')
+const HomeComponent: React.FC = () => {
+  const uploadImage = () => {
+    console.log('upload')
+  }
+
+  const previewImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const targetElement = event.currentTarget as HTMLInputElement
+    const targetFiles = targetElement.files[0]
+    const fileReader = new FileReader()
+
+    fileReader.addEventListener('load', () => {
+      setPreviewImagePath(fileReader.result.toString())
+    })
+
+    fileReader.readAsDataURL(targetFiles)
+  }
+
+  const [previewImagePath, setPreviewImagePath] = useState('')
+    return (
+    <Layout title="画像をアップロードする">
+      <main>
+        <h1>
+          Upload
+        </h1>
+        <Link href="/">
+          <a>戻る</a>
+        </Link>
+        <input type="file" accept="image/*" capture="camera" onChange={previewImage} />
+        <div>
+          <img src={previewImagePath} alt="" />
+        </div>
+        <button type="submit" onClick={uploadImage}>アップロード</button>
+      </main>
+    </Layout>
+  )
 }
-const previewImage = (event: Event) => {
-  const targetElement = event.currentTarget as HTMLInputElement
-  console.log(targetElement.files)
-}
-const HomeComponent: React.FC = () => (
-  <Layout title="画像をアップロードする">
-    <main>
-      <h1>
-        Upload
-      </h1>
-      <Link href="/">
-        <a>戻る</a>
-      </Link>
-      <input type="file" accept="image/*" capture="camera" onChange={previewImage} />
-      <button type="submit" onClick={uploadImage}>アップロード</button>
-    </main>
-  </Layout>
-)
 
 export default HomeComponent
